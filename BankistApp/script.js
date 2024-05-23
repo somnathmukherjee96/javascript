af924516f8f1b4d1ff902eb8ae673bb15b7ed583
Somnath Mukherjee <somnathmukherjee@Somnaths-MBP.bbrouter>
2024-05-24 01:37:04 +0530

    close account

--- BankistApp/script.js
+++ BankistApp/script.js
@@ -184,3 +184,19 @@ btnTransfer.addEventListener("click", function (e) {
     updateUI(currentAccount);
   }
 });
+
+//close account
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin = "";
+});
